951b1af95d1c53259944e5c884039901db031997
drz <qiyunzhu@gmail.com>
2020-11-19 12:13:19 -0700

    added trees collapsed to taxonomic ranks

--- code/scripts/recursive_shear.py
+++ code/scripts/recursive_shear.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
-"""Shear a tree recursively so that eventually all tips match a given taxon
-set.
+"""Shear a tree recursively so that eventually all tips match given taxa.
 
-Internal node labels are considered as taxa, too. Optionally, they cat be
+Internal node labels are considered as taxa, too. Optionally, they can be
 converted to tips branching from their original positions.
 
+Optionally, the branch connecting a collapsed clade will be extended by the
+median depth of all terminal taxa in this clade.
+
 Usage:
-    recursive_shear.py input.nwk taxa.list [0/1] output.nwk
-    1 - convert internal nodes into tips
+    recursive_shear.py input.nwk taxa.list [0/1] [0/1] output.nwk
+
+Flags:
+    #1: Convert internal nodes into tips.
+    #2: Extend branch length by median depth of taxa in collapsed clade.
 """
 
 from sys import argv
+from statistics import median
 from skbio import TreeNode
 
-# whether to convert internal nodes into tips
-int2tip = True if argv[3] == '1' else False
+
+# whether convert internal nodes into tips
+int2tip = argv[3] == '1'
+
+# whether extend collapsed branch length
+exbrlen = argv[4] == '1'
 
 tree = TreeNode.read(argv[1])
 print('Tips in tree: %d.' % tree.count(tips=True))
@@ -29,6 +39,17 @@ print('Taxa in list: %d.' % len(taxa_in_list))
 taxa_to_keep = taxa_in_list.intersection(taxa_in_tree)
 print('Taxa to keep: %d.' % len(taxa_to_keep))
 
+# calculate depths for all nodes
+if exbrlen:
+    for node in tree.postorder():
+        if node.is_tip():
+            node.depths = [0.0]
+            node.mdepth = 0.0
+        else:
+            node.depths = [y + (x.length or 0) for x in node.children
+                           for y in x.depths]
+            node.mdepth = median(node.depths)
+
 # recursively remove tips that are not in the given list
 # the reason for not using scikit-bio's `shear` is because it calls `prune`
 # when done, which collapses single-child internal nodes.
@@ -42,16 +63,24 @@ while True:
             tip.parent.remove(tip)
     else:
         break
+
+# extend branch lengths for collapsed clades
+if exbrlen:
+    for tip in tree.tips():
+        tip.length = (tip.length or 0) + tip.mdepth
+
 print('Remaining tips in tree after shearing: %d.' % tree.count(tips=True))
 
-# converting internal nodes that are in the given list into tips
+# converting internal nodes that are in given list into tips
 if int2tip is True:
     for node in tree.non_tips():
         taxon = node.name
         if taxon is not None and taxon in taxa_to_keep:
             node.name = None
-            node.parent.extend([TreeNode(taxon)])
+            node.parent.extend([TreeNode(
+                taxon, node.mdepth if exbrlen else None)])
     print('Remaining tips in tree after extension: %d.'
           % tree.count(tips=True))
 
-tree.write(argv[4])
+# write output
+tree.write(argv[5])
---
+++ data/trees/ranks/README.md
@@ -0,0 +1,11 @@
+Trees by taxonomic rank
+=======================
+
+Go to [GitHub directory](https://github.com/biocore/wol/tree/master/data/trees/ranks).
+
+Phylogenetic [tree](../tree.nwk) collapsed to taxa at given taxonomic ranks. For example, a "genus tree" is a phylogenetic tree in which every tip is a genus.
+
+Both NCBI and GTDB taxonomies are provided. Because of the incongruence between taxonomy and phylogeny, taxonomy was curated using **tax2tree** to match phylogeny. Some taxon names have an "underscore index" suffix compared with the original names. See [taxonomy](../../taxonomy) for details.
+
+- [**NCBI**](ncbi.tar.bz2)
+- [**GTDB**](gtdb.tar.bz2)
